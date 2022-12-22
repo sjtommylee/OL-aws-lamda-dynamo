82c6cb022dd25750d247a1717eb539d45578c2e6
sjtommylee <sjtommylee@gmail.com>
2022-12-22 12:17:13 -0800

    updating routers

--- posts/routers.go
+++ posts/routers.go
@@ -10,11 +10,14 @@ var err error
 
 func PostRegister(router *gin.RouterGroup) {
 
-	router.GET("/posts/", GetAll)
-	router.GET("/posts/:id", GetSingle)
+	router.GET("/posts", indexAll)
+	router.GET("/posts/:id", index)
+	router.DELETE("/posts/:id", delete)
+	router.PUT("/posts/:id", update)
+
 }
 
-func GetAll(c *gin.Context) {
+func indexAll(c *gin.Context) {
 	// var posts []PostModel
 	if err != nil {
 		fmt.Println("post get all err block")
@@ -25,12 +28,42 @@ func GetAll(c *gin.Context) {
 	}
 }
 
-func GetSingle(c *gin.Context) {
+func index(c *gin.Context) {
 	if err != nil {
 		fmt.Println("get single error block from posts")
 	} else {
 		c.JSON(200, gin.H{
 			"message": "Post get Single",
+		})
+	}
+}
+
+func create(c *gin.Context) {
+	if err != nil {
+		fmt.Println("post create error block")
+	} else {
+		c.JSON(200, gin.H{
+			"message": "Post create",
+		})
+	}
+}
+
+func delete(c *gin.Context) {
+	if err != nil {
+		fmt.Println("post delete error block")
+	} else {
+		c.JSON(200, gin.H{
+			"message": "Post delete",
+		})
+	}
+}
+
+func update(c *gin.Context) {
+	if err != nil {
+		fmt.Println("post update error block")
+	} else {
+		c.JSON(200, gin.H{
+			"message": "Post update",
 		})
 	}
 }
