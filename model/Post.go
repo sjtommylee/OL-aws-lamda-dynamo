21864daadf62c560cce9157f48a52b444f228179
sjtommylee <sjtommylee@gmail.com>
2023-02-21 23:32:04 -0800

    Post

--- model/Post.go
+++ model/Post.go
@@ -1,1 +1,13 @@
 package model
+
+type Post struct {
+	PostId      int64
+	Slug        string
+	Title       string
+	Description string
+	Body        string
+	CreatedAt   int64
+	UpdatedAt   int64
+	Creator     string
+	Dummy       byte // duummy field to just sort by index createdat
+}
