2b9e97158fdce0fcbb03d2b005c3e2721efb2221
sjtommylee <sjtommylee@gmail.com>
2023-02-22 00:58:02 -0800

    test handler

---
+++ route/test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sjtommylee/go-aws-lamda-dynamo/util"
+)
+
+type Response struct {
+	Body string `json:"body"`
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
+
+	response := Response{
+		Body: request.Body,
+	}
+	// return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+	return util.NewSuccessresponse(200, response)
+}
+
+func main() {
+	lambda.Start(Handler)
+}
