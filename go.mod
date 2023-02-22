b52a9288c97b95a086317af9313973f1923922b7
sjtommylee <sjtommylee@gmail.com>
2023-02-22 14:21:52 -0800

    base string util

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/aws/aws-lambda-go v1.37.0
+	github.com/aws/aws-sdk-go v1.44.207
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
--- service/Util.go
+++ service/Util.go
@@ -1,5 +1,23 @@
 package service
 
-import "github.com/aws/aws-sdk-go/service/dynamodb"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
+// define type alias for a map that maps strings to pointers of dynao db attributes
 type AWSObject = map[string]*dynamodb.AttributeValue
+
+// takes in a name, value returns an AWS Object with the provided value
+func StringKey(name, value string) AWSObject {
+	return AWSObject{
+		name: StringValue(value),
+	}
+}
+
+// takes a string input, returns a pointer to the dynamo struct
+func StringValue(value string) *dynamodb.AttributeValue {
+	return &dynamodb.AttributeValue{
+		S: aws.String(value),
+	}
+}
