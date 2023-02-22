7d22a08b9fbc201fe013d69e7130452a8724d778
sjtommylee <sjtommylee@gmail.com>
2023-02-22 01:58:23 -0800

    commons wip

--- service/DB.go
+++ service/DB.go
@@ -1,1 +1,13 @@
 package service
+
+// https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_GetItem.html
+func GetItemByKey() {}
+
+// https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Query.html
+func QueryItems() {}
+
+// https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BatchGetItem.html
+func BatchGetItems() {}
+
+// https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_PutItem.html
+func PutItem() {}
