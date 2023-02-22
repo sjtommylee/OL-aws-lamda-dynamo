283771a526cc302d741b67974d377f6363793601
sjtommylee <sjtommylee@gmail.com>
2023-02-22 13:58:51 -0800

    client

--- service/DynamoDBClient.go
+++ service/DynamoDBClient.go
@@ -1,7 +1,23 @@
 package service
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
 
 // init
 
 var once sync.Once
+var client *dynamodb.DynamoDB
+
+func initializeSingleton() {
+	session := session.Must(session.NewSessionWithOptions(session.Options{}))
+
+	client = dynamodb.New(session)
+}
+
+func DynamoClient() *dynamodb.DynamoDB {
+	return client
+}
