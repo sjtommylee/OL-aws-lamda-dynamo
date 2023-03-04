8f8595c7bf9893fe834aaa91273ba6b2b81fab32
sjtommylee <sjtommylee@gmail.com>
2023-03-03 21:27:07 -0800

    user validations

--- service/UserService.go
+++ service/UserService.go
@@ -1,3 +1,18 @@
 package service
 
-func GetCurrentuser() {}
+import "github.com/sjtommylee/go-aws-lamda-dynamo/model"
+
+func PutUser(user model.User) error {
+	err := user.Validate()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetCurrentuser(auth string) (*model.User, string, error) {
+	// username, token, err := model.VerifyAuth(auth)
+	// return &user, token, nil
+	return nil
+}
