71bb575b13451728140f24eadd34d05085d11b24
sjtommylee <sjtommylee@gmail.com>
2023-03-03 22:25:00 -0800

    util test

---
+++ service/Util_test.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	"testing"
+)
+
+func TestIsUpdateBuilderEmpty(t *testing.T) {
+	// assert.True(t, IsUpdateBuilderEmpty(expression.UpdateBuilder{}))
+	// assert.False(t, IsUpdateBuilderEmpty(expression.Set(expression.Name("Name"), expression.Value("Value"))))
+}
