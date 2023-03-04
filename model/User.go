026bae4f372292ed5fcb4de4545591856f702629
sjtommylee <sjtommylee@gmail.com>
2023-03-03 21:20:07 -0800

    users validation

--- model/User.go
+++ model/User.go
@@ -12,9 +12,20 @@ type User struct {
 }
 
 func (u *User) Validate() error {
+	if u.Username == "" {
+		return NewInputError("username", "cannot be blank")
+	}
+
+	if u.Email == "" {
+		return NewInputError("Email", "cannot be blank")
+	}
+
 	return nil
 }
 
 func ValidatePassword(password string) error {
+	if len(password) < MinLength {
+		return NewInputError("password", "must be specific length")
+	}
 	return nil
 }
